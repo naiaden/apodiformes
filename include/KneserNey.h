0ba936bc54b9fabcdf7acebbb93031ce6289e16e
louis <louis@naiaden.nl>
2013-10-29 17:13:56 +0100

    Fixed KN. I find myself falling for the casted int and double issues in divisions and multiplications...

--- include/KneserNey.h
+++ include/KneserNey.h
@@ -35,7 +35,7 @@ public:
 	void computeFrequencyStats();
 
 private:
-	int n1, n2, n3, n4;
+	double n1, n2, n3, n4;
 
 	/**
 	 * The raw probability is a discounted probability. The n-gram counts are
